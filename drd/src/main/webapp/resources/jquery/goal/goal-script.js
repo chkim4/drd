61b101ce7556be36715a77475532aa9220495640
leeye <leeye@DESKTOP-VNKK1TO>
2023-01-13 13:39:48 +0900

    목표 관리 페이지 추가

---
+++ drd/src/main/webapp/resources/jquery/goal/goal-script.js
@@ -0,0 +1,214 @@
+
+
+
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+function number_format(number, decimals, dec_point, thousands_sep) { 
+  // *     example: number_format(1234.56, 2, ',', ' ');
+  // *     return: '1 234,56'
+  number = (number + '').replace(',', '').replace(' ', '');
+  var n = !isFinite(+number) ? 0 : +number,
+    prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+    sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+    dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+    s = '',
+    toFixedFix = function(n, prec) {
+      var k = Math.pow(10, prec);
+      return '' + Math.round(n * k) / k;
+    };
+  // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+  if (s[0].length > 3) {
+    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+  }
+  if ((s[1] || '').length < prec) {
+    s[1] = s[1] || '';
+    s[1] += new Array(prec - s[1].length + 1).join('0');
+  }
+  return s.join(dec);
+}
+
+// Area Chart Example
+var ctx = document.getElementById("myAreaChart");
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ["월", "화", "수", "목", "금", "토", "일"],
+    datasets: [{
+      label: "목표",
+      lineTension: 0.3,
+      fill: false,
+      borderColor: "rgba(78, 115, 223, 1)",
+      pointRadius: 3,
+      pointBackgroundColor: "rgba(78, 115, 223, 1)",
+      pointBorderColor: "rgba(78, 115, 223, 1)",
+      pointHoverRadius: 3,
+      pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+      pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+      pointHitRadius: 10,
+      pointBorderWidth: 2,
+      data: [0, 10000, 5000, 15000, 10000, 20000, 15000],
+    },
+    {
+      label: "현재",
+      lineTension: 0.3,
+     fill: false,
+      borderColor: "rgba(255, 77, 77, 1)",
+      pointRadius: 3,
+      pointBackgroundColor: "rgba(255, 77, 77, 1",
+      pointBorderColor: "rgba(255, 77, 77, 1)",
+      pointHoverRadius: 3,
+      pointHoverBackgroundColor: "rgba(255, 77, 77, 1",
+      pointHoverBorderColor: "rgba(255, 77, 77, 1)",
+      pointHitRadius: 10,
+      pointBorderWidth: 2,
+      data: [0,2000,10000,1000,5000,10000,5000],
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    layout: {
+      padding: {
+        left: 10,
+        right: 25,
+        top: 10,
+      }
+    },
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'date'
+        },
+        gridLines: {
+          display: false,
+          drawBorder: false
+        },
+        ticks: {
+          maxTicksLimit: 7
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          maxTicksLimit: 5,
+          padding: 10,
+          // Include a dollar sign in the ticks
+          callback: function(value, index, values) {
+            return '$' + number_format(value);
+          }
+        },
+        gridLines: {
+          color: "rgb(234, 236, 244)",
+          zeroLineColor: "rgb(234, 236, 244)",
+          drawBorder: false,
+          borderDash: [2],
+          zeroLineBorderDash: [2]
+        }
+      }],
+    },
+    legend: {
+        position: 'top',
+        labels:{
+		    fontSize : 15,
+		    boxWidth : 10
+	        }
+    },
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      titleMarginBottom: 10,
+      titleFontColor: '#6e707e',
+      titleFontSize: 14,
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      intersect: false,
+      mode: 'index',
+      caretPadding: 10,
+      callbacks: {
+        label: function(tooltipItem, chart) {
+          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+        }
+      }
+    }
+  }
+});
+
+
+//bar차트	
+var context = document
+    .getElementById('myChart')
+    .getContext('2d');
+var myChart = new Chart(context, {
+    type: 'bar', // 차트의 형태
+    data: { // 차트에 들어갈 데이터
+        labels: [
+            //x 축
+            '1','2','3','4','5','6','7'
+        ],
+        datasets: [
+            { //데이터
+                label: '목표 칼로리', //차트 제목
+                fill: false, // line 형태일 때, 선 안쪽을 채우는지 안채우는지
+                data: [
+                    21,19,25,20,23,26,25 //x축 label에 대응되는 데이터 값
+                ],
+                backgroundColor: [
+                    //색상
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    //경계선 색상
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1 //경계선 굵기
+            }/* ,
+        {
+            label: 'test2',
+            fill: false,
+            data: [
+                8, 34, 12, 24
+            ],
+            backgroundColor: 'rgb(157, 109, 12)',
+            borderColor: 'rgb(157, 109, 12)'
+        } */
+        ]
+    },
+    options: {
+    	legend:{
+    		display:false
+    	},
+    	 title: {
+		      display: true,
+		      text: "목표 칼로리",
+		      fontSize : 15,
+		      fontColor : '#9e9e9e'
+		    },
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }
+            ]
+        }
+    }
+});
+
+
+
