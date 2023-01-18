d01c807dcd423b71d350937c8133ac815b6cb90d
leeye <leeye@DESKTOP-VNKK1TO>
2023-01-18 16:31:55 +0900

    pr

--- drd/src/main/java/com/multi/drd/goal/GoalDAO.java
+++ drd/src/main/java/com/multi/drd/goal/GoalDAO.java
@@ -1,17 +1,10 @@
 package com.multi.drd.goal;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-
 import com.multi.drd.member.MemberDTO;
 import com.multi.drd.record.RecordDTO;
-import com.multi.drd.utils.DateUtils;
 
 public interface GoalDAO {
 	GoalDTO readGoal(int memberSEQ);
@@ -22,5 +15,5 @@ public interface GoalDAO {
 	public int updateBodyShape(MemberDTO member);
 	
 	public List<RecordDTO> findByWeek(int memberSEQ);   
-	public int updateProtein(Map<String, Integer> amountAndSeq);   
+	public int updateProtein(Map<String, Integer> amountAndSeq);
 }
