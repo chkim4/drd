c19f9c5e88702e337a343d7cebb5b6f5b8d7b187
ParkSujeong <micem300@naver.com>
2023-01-25 17:04:08 +0900

    # WARNING: head commit changed in the meantime
    Merge remote-tracking branch 'origin/master'
    차트수정, 주석수정

--- drd/src/main/java/com/multi/drd/dashboard/DashboardService.java
+++ drd/src/main/java/com/multi/drd/dashboard/DashboardService.java
@@ -7,12 +7,12 @@ import com.multi.drd.memberbio.MemberBioDTO;
 import com.multi.drd.record.RecordDTO;
 
 public interface DashboardService {
-	/*
-	 * MemberDTO getMemberInfo(int memberSEQ); MemberBioDTO getMemberBio(int
-	 * memberSEQ);
-	 */
+	//월간 주단위 운동시간
 	List<AggregationResultDTO> excerciseHourByWeek(int memberSEQ);
+	//일주일간 운동기록
 	List<RecordDTO> findByWeek(int memberSEQ);
+	//월간 주단위 유산소 운동시간
 	List<AggregationResultDTO> cardioMinByWeek(int memberSEQ);
+	//월간 주단위 무산소 운동시간
 	List<AggregationResultDTO> fitnessMinByWeek(int memberSEQ);
 }
